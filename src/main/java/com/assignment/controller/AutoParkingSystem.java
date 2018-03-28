4a0c24d182a7fd911a70e08d570d79154c86e5b3
narayana <narayana@192-168-1-7.tpgi.com.au>
2018-03-29 00:09:09 +1100

    Assignment

---
+++ src/main/java/com/assignment/controller/AutoParkingSystem.java
@@ -0,0 +1,134 @@
+package com.assignment.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/automatedParking")
+public class AutoParkingSystem {
+	public static final int maxX = 14;
+	public static final int maxY = 14;
+	public Direction direction;
+	public int currentX = 0;
+	public int currentY = 0;
+	
+	private final Logger log = LoggerFactory.getLogger(AutoParkingSystem.class);
+	
+	public enum Direction {
+		NORTH, SOUTH, EAST, WEST;
+	}
+	
+	@GetMapping(value = "/park/{args}")
+	public String park(@PathVariable String args) throws Exception{
+		direction = Direction.NORTH;
+		log.debug("input Arguments : " + args);
+		String[] arguments = args.split(":");
+		if(arguments.length != 2) throw new Exception("Invalid Arrguments");
+		
+		String[] initialXAndY = arguments[0].split(",");
+		if(initialXAndY.length != 2) throw new Exception("Invalid Positions");
+		
+		System.out.println(initialXAndY[0]+initialXAndY[1]);
+		
+		currentX = Integer.parseInt(initialXAndY[0]);
+		currentY = Integer.parseInt(initialXAndY[1]);
+		
+		if(currentX<0 || currentX >14) throw new Exception("X Position should be between 0 to 14");
+		if(currentY<0 || currentY >14) throw new Exception("Y Position should be between 0 to 14");
+		
+		char[] parkingSeq = arguments[1].toCharArray();
+		
+		
+		for(char seq : parkingSeq){
+			System.out.println(seq);
+			switch(seq){
+			case 'f':
+			case 'F':
+				System.out.println("Moving Forward");
+				move();
+				break;
+			case 'r':
+			case 'l':
+			case 'R':
+			case 'L':	
+				System.out.println("Turning " + seq);
+				changeDirection(seq);
+				break;
+			default:
+				throw new Exception("Invalid Sequence: Sequence should be R, L and F");
+			}
+		}
+		System.out.println("Current Position: (" + (currentX) + "," + (currentY) +")");
+		return "Current Position: (" + (currentX) + "," + (currentY) +")";
+	}
+
+	private void changeDirection(char turn) {
+		System.out.println("Current Direction : " + direction);
+		
+		if('R' == turn || 'r' == turn){
+			turnRight();
+			System.out.println("TurninG Right " + direction);
+		}else if('L' == turn || 'l' == turn){
+			turnLeft();
+			System.out.println("TurninG Left " + direction);
+		}
+	}
+
+	private void turnLeft() {
+		switch(direction){
+			case NORTH:
+				direction = Direction.WEST;
+				break;
+			case SOUTH:
+				direction = Direction.EAST;
+				break;
+			case WEST:
+				direction = Direction.SOUTH;
+				break;
+			default:
+				direction = Direction.NORTH;
+		}
+	}
+
+	private void turnRight() {
+		switch(direction){
+			case NORTH:
+				direction = Direction.EAST;
+				break;
+			case SOUTH:
+				direction = Direction.WEST;
+				break;
+			case EAST:
+				direction = Direction.SOUTH;
+				break;
+			default:
+				direction = Direction.NORTH;
+		}
+		
+	}
+
+	private void move() throws Exception{
+		switch(direction){
+			case NORTH:
+				currentX++;
+				if(currentX< 0 || currentX > 14)throw new Exception("Over the boundary");
+				break;
+			case SOUTH:
+				currentX--;
+				if(currentX< 0 || currentX > 14)throw new Exception("Over the boundary");
+				break;
+			case EAST:
+				currentY++;
+				if(currentY< 0 || currentY > 14)throw new Exception("Over the boundary");
+				break;
+			case WEST:
+				currentY--;
+				if(currentY< 0 || currentY > 14)throw new Exception("Over the boundary");
+				break;
+		}
+	}
+}
---
+++ src/test/java/assignment/AutoParkingSystemTest.java
@@ -0,0 +1,40 @@
+package assignment;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.assignment.controller.AutoParkingSystem;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes=AutoParkingSystem.class)
+@AutoConfigureMockMvc
+public class AutoParkingSystemTest {
+	@Autowired
+	private MockMvc mockMvc;
+
+	private final String URL = "/automatedParking/park/";
+	
+	private final Logger log = LoggerFactory.getLogger(AutoParkingSystemTest.class);
+
+	@Test
+	public void TestAutoParkingSystem() throws Exception {
+		log.info("--------- Executing Test");
+		//andDo(MockMvcResultHandlers.print()).
+		mockMvc.perform(MockMvcRequestBuilders.get(URL + "{id}", "5,5:RFLFRFLF")).andExpect(status().isOk()).andExpect(MockMvcResultMatchers.content().string("Current Position: (7,7)"));
+		log.info("--------- --------- ");
+		mockMvc.perform(MockMvcRequestBuilders.get(URL + "{id}", "6,6:FFLFFLFFLFF")).andExpect(status().isOk()).andExpect(MockMvcResultMatchers.content().string("Current Position: (6,6)"));
+		log.info("--------- --------- ");
+		mockMvc.perform(MockMvcRequestBuilders.get(URL + "{id}", "5,5:FLFLFFRFFF")).andExpect(status().isOk()).andExpect(MockMvcResultMatchers.content().string("Current Position: (4,1)"));
+	}
+}
